c55cc8bdeef6b60184a2395532bb0f65692ccf76
Pierre-Emmanuel Diot <p.emmanuel.diot@gmail.com>
2022-02-05 19:58:38 +0100

    Update script.

--- action00.R
+++ action00.R
@@ -1,6 +1,6 @@
 # --------- French Stock - Data Exploration --------
 
-# ----- Setup
+# ----- Setup -----
 
 library(tidyquant)
 library(tidyverse)
@@ -14,76 +14,112 @@ data <- read_csv(file = paste0(backup, "FrenchStocks.csv"))
 tickers <- data %>%
   pull(Symbol)
 
-# ----- First manipulation 
+date_breaks <- c("12m" = "1 month", 
+                 "9m" = "1 month", 
+                 "6m" = "3 weeks", 
+                 "3m" = "2 week", 
+                 "1m" = "3 days", 
+                 "1w" = "1 day")
+periods <- c("2w" = 1, 
+             "1m" = 1, 
+             "3m" = 3, 
+             "6m" = 6, 
+             "9m" = 9, 
+             "12m" = 12)
+
+# ----- Functions -----
+
+get_start_date <- function(period){
+  "Return start date based on period."
+  
+  if ("w" %in% period){
+    start_date <- today() - weeks(periods[period])
+  }
+  else {
+    start_date <- today() - months(periods[period])
+  }
+  return(start_date)
+  
+}
+
+stock_evolution_plot <- function(
+  ticker, 
+  price_data, 
+  period = "12m"
+){
+  "Return evolution plot for given stock and period."
+  
+  start_date <- get_start_date(period = period)
+  title <- paste(ticker, "from", start_date, "to", today())
+  
+  p <- price_data %>%
+    filter(date >= start_date) %>%
+    ggplot(aes(x = date, 
+               y = close)) +
+    geom_line(size = .7, 
+              color = "#709ABE") + 
+    scale_x_date(breaks = date_breaks[period]) +
+    labs(x = "Date", 
+         y = "Close ($)", 
+         title = title) +
+    theme(axis.text.x = element_text(angle = 45))
+  ggplotly(p)
+  
+}
+
+candlestick_plot <- function(
+  ticker, 
+  price_data,
+  period = "12m"
+){
+  "Return candlestick chart for given price data and period."
+  
+  start_date <- get_start_date(period = period)
+  title <- paste(ticker, "from", start_date, "to", today())
+  
+  p <- price_data %>%
+    filter(date >= start_date) %>%
+    mutate(greenRed = ifelse(open - close > 0,
+                             "Red",
+                             "Green")) %>% 
+    ggplot() +
+    geom_segment(aes(x = date,
+                     xend = date,
+                     y = open,
+                     yend = close,
+                     colour = greenRed),
+                 size = 3) +
+    geom_segment(aes(x = date,
+                     xend = date,
+                     y = high,
+                     yend = low,
+                     colour = greenRed)) +
+    geom_line(aes(x = date, 
+                  y = close), 
+              size = .6, 
+              color = "black") +
+    scale_x_date(breaks = date_breaks[period]) +
+    scale_color_manual(values = c("Forest Green", "Red")) +
+    labs(x = "Date", 
+         y = "Close ($)", 
+         title = title) +
+    theme(legend.position ="none",
+          axis.text.x = element_text(angle = 45))
+  
+  ggplotly(p)
+  
+}
 
 
-ticker <- tickers[1]
+# ----- First manipulation -----
+
+ticker <- tickers[2]
 prices <- tq_get(x = ticker, 
                  get = "stock.prices", 
                  from = today() - months(12),
                  to = today(),
                  complete_cases = T)
 
-date_breaks <- c("1m" = "1 month", 
-                 "2m" = "2 months", 
-                 "2w" = "2 weeks", 
-                 "1w" = "1 week", 
-                 "3d" = "3 days")
-periods <- c("1m" = 1, 
-             "3m" = 3, 
-             "6m" = 6, 
-             "9m" = 9, 
-             "12m" = 12)
+stock_evolution_plot(ticker = ticker, price_data = prices)
+candlestick_plot(ticker = ticker, price_data = prices)
 
-# stock evolution plot
-start_date <- today() - months(periods["1m"])
-title <- paste(ticker, "from", start_date, "to", today())
-
-p1 <- prices %>%
-  filter(date >= start_date) %>%
-  ggplot(aes(x = date, 
-             y = close)) +
-  geom_line(size = .7, 
-            color = "#709ABE") + 
-  scale_x_date(breaks = date_breaks["3d"]) +
-  labs(x = "Date", 
-       y = "Close ($)", 
-       title = title) +
-  theme(axis.text.x = element_text(angle = 45))
-
-ggplotly(p1)
-
-# candlestick plot 
-start_date <- today() - months(periods["3m"])
-title <- paste(ticker, "from", start_date, "to", today())
-
-p2 <- prices %>%
-  filter(date >= start_date) %>%
-  mutate(greenRed=ifelse(open-close>0,
-                         "Red",
-                         "Green")) %>% 
-  ggplot() +
-  geom_segment(aes(x = date,
-                   xend = date,
-                   y = open,
-                   yend = close,
-                   colour = greenRed),
-               size = 3) +
-  geom_segment(aes(x = date,
-                   xend = date,
-                   y = high,
-                   yend = low,
-                   colour = greenRed)) +
-  geom_line(aes(x = date, 
-                y = close), 
-            size = .6, 
-            color = "black") +
-  scale_x_date(breaks = date_breaks["1w"]) +
-  scale_color_manual(values = c("Forest Green", "Red")) +
-  labs(x = "Date", 
-       y = "Close ($)", 
-       title = title) +
-  theme(legend.position ="none",
-        axis.text.x = element_text(angle = 45))
-
-ggplotly(p2)
