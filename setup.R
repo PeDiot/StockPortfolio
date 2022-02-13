fddca725b2ffaf09c1b2cd9e86c883f9de560ba6
Pierre-Emmanuel Diot <p.emmanuel.diot@gmail.com>
2022-02-13 20:27:44 +0100

    Functions and utils to build the app.

--- setup.R
+++ setup.R
@@ -4,17 +4,22 @@ options(warn=-1) # turn warnings off
 
 # ----- French companies data -----
 backup <- "./backup/"
-data <- read_csv(file = paste0(backup, "FrenchStocks.csv"), 
+french_stocks <- read_csv(file = paste0(backup, "FrenchStocks.csv"), 
                  show_col_types = FALSE)
 
 # ----- Colors -----
 
-asset <- "#709ABE"
+evolution <- "#709ABE"
 high <- "Forest Green"
 low <- "Red"
 short <- "purple"
 long <- "black"
+macd <- "#C47D72"
+rsi <- "#F3B0DE"
 
+# ----- GGplot theme -----
+
+theme_set(theme_minimal())
 
 # ----- Dates -----
 
@@ -23,19 +28,29 @@ periods <- c("2w" = 1,
              "3m" = 3, 
              "6m" = 6, 
              "9m" = 9, 
-             "12m" = 12)
+             "12m" = 12, 
+             "24m" = 24)
 
-date_breaks <- c("12m" = "1 month", 
-                 "9m" = "3 weeks", 
-                 "6m" = "2 weeks", 
-                 "3m" = "1 week", 
-                 "1m" = "3 days", 
-                 "2w" = "2 days")
+# ----- Utils -----
 
-# ----- Functions -----
+get_asset <- function(ticker){
+  "Return ticker for a given asset."
+  
+  french_stocks %>%
+    filter(Symbol == ticker) %>%
+    pull(Name)
+}
 
-# --- Utils
-get_start_date <- function(period){
+get_ticker <- function(asset){
+  "Return asset for a given ticker."
+  
+  french_stocks %>%
+    filter(Name == asset) %>%
+    pull(Symbol) 
+}
+
+
+get_start_date <- function(period, price_data){
   "Return start date based on period."
   
   if ("w" %in% period){
@@ -44,242 +59,78 @@ get_start_date <- function(period){
   else {
     start_date <- today() - months(periods[period])
   }
-  return(start_date)
+  min_date <- price_data %>%
+    pull(date) %>%
+    min()
+  if (start_date < min_date){
+    return(min_date)
+  }
+  else{ return(start_date) }
   
 }
 
-get_tq_data <- function(company, num_months = 12){
-  "Return price data for given company and period."
-  
-  ticker <- data %>%
-    filter(Name == company) %>%
-    pull(Symbol)
-  tq_get(x = ticker, 
-         get = "stock.prices", 
-         from = today() - months(num_months),
-         to = today(),
-         complete_cases = T)
-  
-}
-
-# --- Data Viz
 get_title <- function(ticker, start_date){
-  
+  "Make title."
+  asset <- get_asset(ticker)
   title <- paste0(ticker, 
-                  " (", 
-                  company,
-                  ") from ",
-                  start_date,
-                  " to ",
-                  today())
+                 " (", 
+                 asset,
+                 "): ", 
+                 start_date, 
+                 " / ", 
+                 today())
+  return(title)
+}
+
+get_tq_data <- function(tickers, num_months = 12){
+  "Return price data for given asset and period."
+  
+  if (length(tickers) == 1){
+    tq_get(x = tickers, 
+           get = "stock.prices", 
+           from = today() - months(num_months),
+           to = today(),
+           complete_cases = T)
+  }
+  else {
+    dat <- tq_get(x = tickers, 
+                  get = "stock.prices", 
+                  from = today() - months(num_months),
+                  to = today(),
+                  complete_cases = T)
+    colnames(dat)[1] <- "ticker"  
+    df_list <- split(dat, dat$ticker)
+    return(df_list)
+  }
+  
   
 }
 
-stock_evolution_plot <- function(
-  company, 
-  price_data, 
-  period = "12m"
-){
-  "Return evolution plot for given company and period."
+# ----- Portfolio Value -----
+
+get_portfolio_value <- function(data, num_shares){
+  "Return portfolio value given assets' prices and number of shares."
   
-  start_date <- get_start_date(period = period)
-  ticker <- data %>%
-    filter(Name == company) %>%
-    pull(Symbol)
+  dat <- lapply(
+    data, 
+    function(df){
+      ticker <- df %>%
+        pull(ticker) %>%
+        unique()
+      df %>%
+        mutate(value = close * num_shares[ticker]) 
+    }
+  ) %>%
+    bind_rows()
   
-  p <- price_data %>%
-    filter(date >= start_date) %>%
-    ggplot(aes(x = date, 
-               y = close)) +
-    geom_line(size = .7, 
-              color = asset) + 
-    scale_x_date(breaks = date_breaks[period]) +
-    labs(x = "Date", 
-         y = "Close ($)", 
-         title = get_title(ticker, start_date)) +
-    theme(axis.text.x = element_text(angle = 45))
-  ggplotly(p)
+  dat %>%
+    select(c(date, value)) %>%
+    group_by(date) %>%
+    summarize(value = sum(value))
   
 }
 
-candlestick_plot <- function(
-  company, 
-  price_data,
-  period = "12m"
-){
-  "Return candlestick chart for given company and period."
-  
-  start_date <- get_start_date(period = period)
-  ticker <- data %>%
-    filter(Name == company) %>%
-    pull(Symbol)
-  
-  p <- price_data %>%
-    filter(date >= start_date) %>%
-    mutate(greenRed = ifelse(open - close > 0,
-                             "Red",
-                             "Green")) %>% 
-    ggplot() +
-    geom_segment(aes(x = date,
-                     xend = date,
-                     y = open,
-                     yend = close,
-                     colour = greenRed),
-                 size = 3) +
-    geom_segment(aes(x = date,
-                     xend = date,
-                     y = high,
-                     yend = low,
-                     colour = greenRed)) +
-    geom_line(aes(x = date, 
-                  y = close), 
-              size = .5, 
-              color = "black", 
-              linetype = "dashed") +
-    scale_x_date(breaks = date_breaks[period]) +
-    scale_color_manual(values = c(high, low)) +
-    labs(x = "Date", 
-         y = "Close ($)", 
-         title = get_title(ticker, start_date)) +
-    theme(legend.position ="none",
-          axis.text.x = element_text(angle = 45))
-  
-  ggplotly(p)
-  
-}
-  
-
-moving_avg_plot <- function(
-  company, 
-  price_data, 
-  MAshort = "MA20", 
-  MAlong = "MA50", 
-  period = "12m"
-){
-  "Return a chart with price and moving averages."
-  
-  start_date <- get_start_date(period = period)
-  ticker <- data %>%
-    filter(Name == company) %>%
-    pull(Symbol)
-  sells <- price_data %>%
-    filter(date >= start_date
-           & MASell == 1) 
-  buys <- price_data %>%
-    filter(date >= start_date
-           & MABuy == 1) 
-  
-  p <- price_data %>%
-    filter(date >= start_date) %>%
-    ggplot(aes(x = date)) +
-    geom_line(aes(y = close, 
-                  colour = "Close"), 
-              size = .7) +
-    geom_line(aes_string(y = MAshort, 
-                         colour = shQuote(MAshort)), 
-              linetype = "dashed", 
-              size = .5) +
-    geom_line(aes_string(y = MAlong, 
-                         colour = shQuote(MAlong)),
-              linetype = "dashed", 
-              size = .5) +
-    geom_point(data = sells, 
-               aes(x = date, 
-                   y = close, 
-                   colour = "Selling signals"), 
-               shape = 25,
-               size = 3) +
-    geom_point(data = buys, 
-               aes(x = date, 
-                   y = close, 
-                   colour = "Buying signals"), 
-               shape = 24,
-               size = 3) +
-    scale_x_date(breaks = date_breaks[period]) +
-    scale_color_manual("Value",
-                       values = c("Close" = asset, 
-                                  MAshort = short,
-                                  MAlong = long,
-                                  "Selling signals" = low, 
-                                  "Buying signals" = high)) +
-    labs(x = "Date",
-         y = "Value ($)",
-         title = get_title(ticker, start_date)) +
-    theme(legend.title = element_blank(), 
-          legend.position = "bottom", 
-          axis.text.x = element_text(angle = 45)) 
-  
-  ggplotly(p) %>%
-    layout(legend = list(orientation = "h", 
-                         x = .3, 
-                         y = -0.2, 
-                         title = "none"))
-  
-}
-
-macd_plot <- function(
-  company, 
-  price_data, 
-  period = "12m"
-){
-  "Plot MACD and signal line."
-  
-  start_date <- get_start_date(period = period)
-  ticker <- data %>%
-    filter(Name == company) %>%
-    pull(Symbol)
-  sells <- price_data %>%
-    filter(date >= start_date
-           & MACDSell == 1) 
-  buys <- price_data %>%
-    filter(date >= start_date
-           & MACDBuy == 1)
-  
-  p <- price_data %>%
-    filter(date >= start_date) %>%
-    ggplot(aes(x = date)) +
-    geom_line(aes(y = MACD, 
-                  color = "MACD"), 
-              size = .6) +
-    geom_line(aes(y = Signal, 
-                  color = "Signal"), 
-              size = .5, 
-              linetype = "dashed") +
-    geom_point(data = sells, 
-               aes(x = date, 
-                   y = Signal, 
-                   colour = "Selling signals"), 
-               shape = 25,
-               size = 3) +
-    geom_point(data = buys, 
-               aes(x = date, 
-                   y = Signal, 
-                   colour = "Buying signals"), 
-               shape = 24,
-               size = 3) +
-    scale_x_date(breaks = date_breaks[period]) +
-    scale_color_manual("Value",
-                       values = c("MACD" = asset,
-                                  "Signal" = long, 
-                                  "Selling signals" = low, 
-                                  "Buying signals" = high)) +
-    labs(x = "Date",
-         y = "% change",
-         title = get_title(ticker, start_date)) +
-    theme(legend.title = element_blank(), 
-          legend.position = "bottom", 
-          axis.text.x = element_text(angle = 45)) 
-  
-  ggplotly(p) %>%
-    layout(legend = list(orientation = "h", 
-                         x = .35, 
-                         y = -0.2, 
-                         title = "none"))
-
-}
-
-
-# --- Indicators
+# ----- Financial indicators -----
 
 add_moving_avg <- function(
   price_data,
@@ -358,6 +209,7 @@ add_macd <- function(
 }
 
 get_macd_signals <- function(price_data){
+  "Identify buying and selling signals from MACD."
   
   col_names <- colnames(price_data)
   if ("MACD" %in% col_names & "Signal" %in% col_names){
@@ -374,7 +226,256 @@ get_macd_signals <- function(price_data){
       )
   }
   else{
-    stop("You need to calculate MACD and MACD signal.")
+    stop("You need to calculate MACD and signal.")
   }
   
 }
+
+
+add_rsi <- function(
+  price_data, 
+  rsi_period = 10, 
+  ma_type = "WMA"
+){
+  "Compute Relative Strength Index (RSI) for given period and prices."
+  
+  price_data %>%
+    mutate(RSI = RSI(price = close, 
+                     n = 14, 
+                     maType = ma_type))
+  
+}
+
+get_rsi_signals <- function(
+  price_data,
+  lower_thresold = 30, 
+  upper_thresold = 70
+){
+  "Identify buying and selling signals from RSI."
+  
+  if ("RSI" %in% colnames(price_data)){
+    price_data %>%
+      mutate(
+        BuyRSI = case_when(
+          RSI < lower_thresold ~ 1, 
+          TRUE ~ 0
+        ), 
+        SellRSI = case_when(
+          RSI > upper_thresold ~ 1, 
+          TRUE ~ 0
+        )
+      )
+  }
+  else{
+    stop("You need to calculate RSI.")
+  }
+  
+}
+
+# ----- Data Viz -----
+
+range_selector_period <- function(x_pos = .5, y_pos){
+  "Plotly buttons to select period."
+  
+  list(visible = TRUE, x = x_pos, y = y_pos,
+       xanchor = "center", yref = "paper",
+       font = list(size = 9),
+       buttons = list(
+         list(count=1,
+              label="ALL",
+              step="all"),
+         list(count=6,
+              label="6 MO",
+              step="month",
+              stepmode="backward"),
+         list(count=3,
+              label="3 MO",
+              step="month",
+              stepmode="backward"),
+         list(count=1,
+              label="1 MO",
+              step="month",
+              stepmode="backward")
+       ))
+  
+}
+
+candlestick_chart <- function(price_data, legend_group = "one"){
+  "Build plotly candlestick chart for a given asset."
+  
+  p <- plot_ly(price_data) %>%
+    add_trace(type = "candlestick",
+              x = ~date,
+              open = ~open, 
+              high = ~high, 
+              low = ~low, 
+              close = ~close,
+              name = "Close", 
+              increasing = list(line = list(color = high,
+                                            width = 1.5)), 
+              decreasing = list(line = list(color = low,
+                                            width = 1.5)), 
+              legendgroup = legend_group)
+}
+
+add_moving_averages_trace <- function(plot, legend_group = "one"){
+  "Add MA20 and MA50."
+  
+  plot %>%
+    add_trace(type = "scatter", 
+              mode = "lines",
+              marker = NULL,
+              x = ~date,
+              y = ~MA20,
+              name = "MA20",
+              yaxis = "y1", 
+              line = list(color = short, 
+                          width = .7),
+              legendgroup = "one", 
+              hoverinfo = "none", 
+              legend_group = legend_group) %>%
+    add_trace(type = "scatter", 
+              mode = "lines",
+              marker = NULL,
+              x = ~date,
+              y = ~MA50,
+              name = "MA50",
+              yaxis = "y1", 
+              line = list(color = long, 
+                          dash = "dot", 
+                          width = .7), 
+              legendgroup = legend_group)
+}
+
+add_macd_trace <- function(plot, legend_group = "two"){
+  "Add MACD and MACD signal on a new chart."
+  
+  plot %>%
+    add_trace(type = "scatter", 
+              mode = "lines",
+              marker = NULL,
+              x = ~date,
+              y = ~MACD,
+              name = "MACD",
+              yaxis = "y2", 
+              line = list(color = macd,
+                          width = 1.3), 
+              legendgroup = legend_group) %>%
+    add_trace(type = "scatter", 
+              mode = "lines",
+              marker = NULL,
+              x = ~date,
+              y = ~Signal,
+              name = "MACD Signal",
+              yaxis = "y2", 
+              line = list(color = long,
+                          width = 1,
+                          dash = "dot"), 
+              legendgroup = legend_group) 
+}
+
+add_rsi_trace <- function(plot, legend_group = "three"){
+  "Add RSI signal and bounds on a new chart."
+  
+  plot %>% 
+    add_trace(type = "scatter", 
+              mode = "lines",
+              marker = NULL,
+              x = ~date,
+              y = ~RSI,
+              name = "RSI",
+              yaxis = "y3", 
+              line = list(color = rsi,
+                          width = 1.5), 
+              legendgroup = legend_group) %>%
+    add_trace(type = "scatter", 
+              mode = "lines",
+              marker = NULL,
+              x = c(~min(date), ~max(date)),
+              y = c(70,70),
+              name = "Upper RSI (70)",
+              line = list(color = "red",
+                          width = 0.5,
+                          dash = "dot"),
+              yaxis = "y3", 
+              legendgroup = legend_group) %>%
+    add_trace(type = "scatter", 
+              mode = "lines",
+              marker = NULL,
+              x = c(~min(date), ~max(date)),
+              y = c(30,30),
+              name = "Lower RSI (30)",
+              line = list(color = "red",
+                          width = 0.5,
+                          dash = "dot"),
+              yaxis = "y3", 
+              legendgroup = legend_group) 
+}
+
+indicators_plot <- function(price_data, title){
+  "Make plotly layout."
+  price_data %>%
+    candlestick_chart() %>%
+    add_moving_averages_trace() %>%
+    add_macd_trace() %>% 
+    add_rsi_trace() %>%
+    layout(title = title,
+           xaxis = list(rangeslider = list(visible = F), 
+                        rangeselector = range_selector_period(y_pos = -0.055), 
+                        title = ""),
+           yaxis = list(domain = c(0.68, 1),
+                        fixedrange = FALSE, 
+                        title = "$"),
+           yaxis2 = list(domain = c(0.32, 0.58),
+                         fixedrange = FALSE, 
+                         title = "%"), 
+           yaxis3 = list(domain = c(0., 0.28),
+                         fixedrange = FALSE, 
+                         title = ""),
+           legend = list(orientation = "h", x = 0.5, y = -.07,
+                         xanchor = "center", yref = "paper",
+                         font = list(size = 12),
+                         bgcolor = "transparent", 
+                         borderwidth = .2)) 
+}
+
+portfolio_evolution <- function(portfolio_data, title){
+  "Plot portfolio value evolution."
+  
+  plot_ly(portfolio_data) %>%
+    add_trace(type = "scatter", 
+            mode = "lines",
+            marker = NULL,
+            x = ~date,
+            y = ~value,
+            line = list(color = evolution,
+                        width = 1.5)) %>%
+    layout(title = title, 
+           xaxis = list(rangeslider = list(visible = F), 
+                        rangeselector = range_selector_period(y_pos = -.15), 
+                        title = ""), 
+           yaxis = list(title = "$")) 
+  
+}
+
+# ----- UI -----
+
+num_shares_input <- function(asset1, asset2){
+  "Build shiny numeric input for number of shares."
+  
+  inputId1 <- paste("num_shares", get_ticker(asset1), sep = "_")
+  inputId2 <- paste("num_shares", get_ticker(asset2), sep = "_")
+  fluidRow(
+    column(width = 6, 
+           numericInput(inputId1,
+                        label = h5(asset1), 
+                        min = 0, 
+                        value = 1)), 
+    column(width = 6,
+           numericInput(inputId2, 
+                        label = h5(asset2), 
+                        min = 0, 
+                        value = 1))
+  )
+}
+  
