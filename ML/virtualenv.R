f29bf0524609035eb9b26c1a23f6d6c72daa5e4c
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2022-03-03 17:46:20 +0100

    Change virtual environment location.

--- ML/virtualenv.R
+++ ML/virtualenv.R
@@ -4,7 +4,7 @@ setwd("./ML/")
 
 # NOT RUN {
 
-  virtualenv_create(envname = "stockPrediction_virtualenv",
+  virtualenv_create(envname = "./stockPrediction_virtualenv",
                     python= "C:\\Users\\pemma\\AppData\\Local\\Programs\\Python\\Python39\\python.exe")
 
   virtualenv_install("stockPrediction_virtualenv",
